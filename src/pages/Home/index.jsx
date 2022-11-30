573720cd1c2f3e000ac6679084b1d65a34754a5d
HerbertAms <herbert.ams@gmail.com>
2022-11-30 14:33:26 -0300

    insert newStudents

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Card} from '../../components/Card'
 
 import './style.css'
@@ -6,20 +6,50 @@ import './style.css'
 
 export function Home() {
   const [studentName, setStudentName] = useState()
+  const [students, setStudents] = useState([])
+
+  function handleAddStudent(){
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br",{
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+
+    };
+
+    
+    setStudents(prevState => [...prevState, newStudent]) 
+  }
 
   return (
     <div className='container'>
-      <h1>Nome: {studentName}</h1>
+      <header>
+          <h1>Lista de presença</h1>
+        <div>
+          <strong>Herbert Severino</strong>
+          <img src="https://github.com/HerbertAMS.png" alt="Foto de perfil" />
+        </div>
+      </header>
       <input 
         type="text" 
         placeholder="Digite o seu nome..." 
         onChange={e =>  setStudentName(e.target.value)}
       />
-      <button type="button">Adicionar</button>
+      <button type="button" onClick={handleAddStudent}>
+        Adicionar
+      </button>
       
-      <Card name="Herbert Severino" time="11:34:02"/>
-      <Card name="Geise Severino" time="12:05:59"/>
-      <Card name="Margarida Moreira" time="12:09:05"/>
+      {
+        students.map(student => 
+        <Card 
+        key={student.time}
+        name={student.name} 
+        time={student.time}
+        />)
+      }
+
     </div>
   )
 }
